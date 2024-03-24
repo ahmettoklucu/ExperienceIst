1bfc30770f0713c577705299b5dae87458191493
ahmettoklucu <toklucuahmet900@gmail.com>
2024-03-24 23:28:34 +0300

    Database nesnelerinin oluşturulmasi

---
+++ ExperienceIst.Entities/Concrate/Request.cs
@@ -0,0 +1,36 @@
+﻿using ExperienceIst.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExperienceIst.Entities.Concrate
+{
+    public class Request:IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EMail { get; set; }
+        public int  RoomNumber { get; set; }
+        public bool Hair { get; set; }
+        public bool Skin { get; set; }
+        public bool Teeth { get; set; }
+        public bool PcpOther { get; set; }
+        public string OtherPcbDetail { get; set; }
+        public bool IstanbulTour { get; set; }
+        public bool TraditionalProduct { get; set; }
+        public bool TpOther { get; set; }
+        public string TpOtherDetail { get;set; }
+        public bool Taxi { get; set; }
+        public bool VipCar { get; set; }
+        public bool MobilePhone { get; set; }
+        public bool LoptopCharger { get; set; }
+        public string DetailsAndOthers { get; set; }
+        public bool Status { get; set; }
+        public DateTime SaveDate { get; set; } = DateTime.Now;
+        public DateTime ConfirmDate { get; set; } = DateTime.Now;
+    }
+}
---
+++ ExperienceIst.Entities/Concrate/User.cs
@@ -0,0 +1,16 @@
+﻿using ExperienceIst.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExperienceIst.Entities.Concrate
+{
+    public class User:IEntity
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
