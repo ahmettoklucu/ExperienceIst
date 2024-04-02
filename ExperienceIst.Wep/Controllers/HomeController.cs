c698b6d7bb019aebf140b2ca62a8e730ac1e796e
ahmettoklucu <toklucuahmet900@gmail.com>
2024-04-02 23:25:17 +0300

    init

---
+++ ExperienceIst.Wep/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExperienceIst.Wep.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MakeRequest()
+        {
+            return View();
+        }
+    }
+}
